3096e926f10e06308b35d06d15845f42160546d7
Garrett Adams <garrett.adams52@gmail.com>
2022-05-25 17:06:44 -0400

    Finished python

--- deaf_grandma.py
+++ deaf_grandma.py
@@ -1,5 +1,26 @@
+from curses.ascii import isupper
+
+
 def deaf_grandma():
+    
+    bye = 0
 
+    while bye != 2:
+        string = input()
 
+        if string == '':
+            print("WHAT?!")
+        elif (string.isupper() and string != 'GOODBYE!'):
+            print('NO, NOT SINCE 1946!')
+        elif string == 'GOODBYE!':
+            if bye == 0:
+                print('LEAVING SO SOON?')
+                bye = bye + 1
+            else:
+                print('LATER, SKATER!')
+                bye = bye + 1
+        else:
+            print('SPEAK UP, KID!')
+    
 
-    deaf_grandma()
+deaf_grandma()
